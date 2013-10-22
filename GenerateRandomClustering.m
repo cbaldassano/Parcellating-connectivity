86c03804b1d90b81ad3b026d1782888332e64c41
Chris Baldassano <chrisb33@cs.stanford.edu>
2013-10-22 16:23:54 -0700

    Minor change to interface of random cluster generation. This code used to start 5-pass run with sigsq=10.

--- GenerateRandomClustering.m
+++ GenerateRandomClustering.m
@@ -2,13 +2,18 @@ function [z c] = GenerateRandomClustering( adj_list, K )
 
     N = length(adj_list);
     z = 1:N;
-    while (length(unique(z)) > K)
+    K_curr = N;
+    while (K_curr > K)
+        if (mod(K_curr, 1000) == 0)
+            disp(K_curr);
+        end
         clustLabels = unique(z);
         zToMerge = clustLabels(randi(length(clustLabels),1));
         mergeChoices = unique(z([adj_list{z==zToMerge}]));
         mergeChoices = setdiff(mergeChoices,zToMerge);
         if (~isempty(mergeChoices))
             z(z==zToMerge) = mergeChoices(randi(length(mergeChoices),1));
+            K_curr = K_curr - 1;
         end
     end
 
@@ -19,6 +24,8 @@ function [z c] = GenerateRandomClustering( adj_list, K )
         relabelInd = relabelInd+1;
     end
     z = zRelabel;
-    c = ClusterSpanningTrees(z, adj_list);
+    if (nargout == 2)
+        c = ClusterSpanningTrees(z, adj_list);
+    end
 end
 
