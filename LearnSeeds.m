b4f9eb9257f1bbd323485a8f60fd0be73d859308
Chris Baldassano <chrisb33@cs.stanford.edu>
2013-10-29 11:43:38 -0700

    Allowed for num_seeds not equal to 10

--- LearnSeeds.m
+++ LearnSeeds.m
@@ -1,8 +1,9 @@
 function LearnSeeds(sigsq)
 
-map_z = cell(10,1);
-stats = cell(10,1);
-parfor seed = 1:10
+num_seeds = 5;
+map_z = cell(num_seeds,1);
+stats = cell(num_seeds,1);
+parfor seed = 1:num_seeds
     rng(seed); 
     [map_z{seed} stats{seed}] = ddCRP('unrelated40','full',5,10,0.0001,1,sigsq,1000,1);
 end
